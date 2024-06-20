a71da9e9b3a712407a00d27afd90315d11fc1319
Karthikeyan M <karthikeyanm@Karthikeyans-MacBook-Air.local>
2024-06-20 08:35:09 +0530

    feat(spring):add sprign first project

---
+++ src/test/java/com/karthick/hello_spring/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthick.hello_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
